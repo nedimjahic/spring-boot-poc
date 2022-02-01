4f6d46a9263a9c2e0d5d3cb7d33fc03ecd00da6a
Nedim Jahic <nedimjahic@live.com>
2022-02-01 23:11:08 +0100

    Set up project structure, added Spring and Lombok dependencies.

---
+++ src/test/java/com/acme/poc/SpringPocApplication/SpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.poc.SpringPocApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
